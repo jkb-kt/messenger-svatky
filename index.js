7167011c06f7a0afcf41d9f5963b4fa61ce3ea1e
Mosses <mr.bukaj@gmail.com>
2018-03-17 17:48:46 +0100

    test

--- index.js
+++ index.js
@@ -30,9 +30,21 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook/', function(req, res) {
     let messaging_events = req.body.entry[0].messaging
+    setInterval(function() {
+        for (let i = 0; i < messaging_events.length; i++) {
+            let event = messaging_events[i]
+            let sender = event.sender.id
+           // let sender = 784619495065508
+            if (event.message && event.message.text) {
+                let text = event.message.text
+                sendText(sender, "Text echo: " + text.substring(0, 100))
+            }
+        }
+    },2000);
     for (let i = 0; i < messaging_events.length; i++) {
         let event = messaging_events[i]
         let sender = event.sender.id
+       // let sender = 784619495065508
         if (event.message && event.message.text) {
             let text = event.message.text
             sendText(sender, "Text echo: " + text.substring(0, 100))
